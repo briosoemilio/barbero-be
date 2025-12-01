90fef33b06054585b05f8def689279a2d9dba91e
develmo <brioso.emilioluis@gmail.com>
2025-12-01 12:25:54 +0800

    add and apply lint to hello world function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,22 @@
-import { setGlobalOptions } from "firebase-functions";
-import { onRequest } from "firebase-functions/https";
+import {setGlobalOptions} from "firebase-functions";
+import {onRequest} from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
 import express from "express";
 import cors from "cors";
 
-setGlobalOptions({ maxInstances: 10 });
+setGlobalOptions({maxInstances: 10});
 
 // initialize app
 const app = express();
 
 // enable cors
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 app.use(express.json());
 
 // routes
 app.get("/hello", (_, res) => {
-	logger.info("Hello logs!", { structuredData: true });
-	res.send("Hello barbero app")
-})
+  logger.info("Hello logs!", {structuredData: true});
+  res.send("Hello barbero app");
+});
 
 exports.api = onRequest(app);
